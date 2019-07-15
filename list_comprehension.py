b501cd62d5e6e270b4ccb80c6b0f3d274bbb8e1f
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-15 18:39:54 +0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -23,7 +23,7 @@ print(res)
 #   list of words ["hello", "python", "proffesional", "world"]
 # create a list of 100 random numbers 
 #   1: 1-10 2:1-11 3: 1-12 ...
-# matrix = [[1,2,3], [4,5,6], [7,8,9]
+# matrix = [[1,2,3], [4,5,6], [7,8,9]]
 #   create a list of all sub list in position 0
 # create a list of even numbers between 1 and 200
 # input 5 numbers for the user into a list
