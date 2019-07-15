3ca37b9c82987905b1eac754ff1a11a968f7a749
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-15 20:21:45 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,18 @@
 
-
-popMap = { 'TEL AVIV' : 443939, 'LONDON' : 8825000, 'PARIS' : 0,'TOKYO' : 13929286}
-
 # input city name from user
 # check if city in keys
 #   if yes print population
-#   if not print "not exist" and ask for population from the user
-#   then add to dictionary
-#   do it in while loop till get 'Exit' in city name
+#   if not print - not exist - ask for pop from user add to dict
+#   in while loop till get 'Exit' in city name
+
+popMap = { 'TEL AVIV' : 443939, 'LONDON' : 8825000, 'PARIS' : 0,'TOKYO' : 13929286}
+city = input("Enter city name: ")
+while city.upper() != 'EXIT':
+    if city.upper() in popMap.keys():
+        print(popMap[city.upper()])
+    else:
+        pop = int(input(f"Enter number of citizens of {city}: "))
+        popMap[city.upper()] = pop
+    city = input("Enter city name: ")
+
+
