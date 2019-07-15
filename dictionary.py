0b370dd86c1af1092d5352b32b61897ab7cb100c
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-15 19:45:29 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+
+
+popMap = { 'TEL AVIV' : 443939, 'LONDON' : 8825000, 'PARIS' : 0,'TOKYO' : 13929286}
+
+# input city name from user
+# check if city in keys
+#   if yes print population
+#   if not print - not exist - ask for pop from user add to dict
+#   in while loop till get 'Exit' in city name
